a93faad9d5e9ac26447795f77a277bb1fa8c3962
saumyapanda1 <saumya.iisc@gmail.com>
2025-03-25 06:12:35 +0530

    Time: 3 ms (30.25%), Space: 11.7 MB (24%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> v; 
+        ps(v, 0, nums);
+
+        return v; 
+        
+    }
+
+    void ps(vector<vector<int>>&v, int pos, vector<int>  nums)
+    {
+        if(pos >= nums.size())
+        {
+            v.push_back(nums);
+            return; 
+        }
+
+        for(int i=pos; i<nums.size(); i++)
+        {
+            swap(nums[pos], nums[i]);
+            ps(v, pos+1, nums);
+            swap(nums[pos], nums[i]);
+        }
+        
+    }
+};
